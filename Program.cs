a0ba6d603547b888d6e1827af3e53f40a2b05d34
TOMAS BLAUSTEIN <01BL41557724@campus.economicas.uba.ar>
2021-03-16 16:46:04 -0300

    .

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace A892569.Actividad01
     {
         static void Main(string[] args)
         {
-            Console.Write("Hola mundo!");
+            Console.WriteLine("Hola mundo!");
             Console.WriteLine("Presione cualquier tecla para salir.");
             Console.ReadKey();
         }
